d837ceb5362bfcdffa750664e9c61abca8a7ec54
Farley Piedrahita Orozco <farchodev@gmail.com>
2025-07-23 00:14:01 -0500

    feat: Mejora la página de inicio de sesión con un diseño más atractivo y mensajes de bienvenida

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { SignIn } from '@clerk/nextjs';
 
 export default function SignInPage() {
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className='flex flex-col items-center justify-center gap-4 p-4'>
+        <h1 className='font-semibold text-4xl'>Welcome back!</h1>
+        <p className='text-xl'>Sign in to your account</p>
+
+        <SignIn />
     </div>
   )
 }
